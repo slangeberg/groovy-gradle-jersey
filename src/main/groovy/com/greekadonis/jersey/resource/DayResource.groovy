3e312f2a4024dd0725ce8de1e1b1163b7ce80ce4
Scott Langeberg <slangeberg@gmail.com>
2014-06-10 02:41:27 +0100

    added query by path param

--- src/main/groovy/com/greekadonis/jersey/resource/DayResource.groovy
+++ src/main/groovy/com/greekadonis/jersey/resource/DayResource.groovy
@@ -4,14 +4,15 @@ import com.greekadonis.jersey.domain.Day
 import org.json.JSONArray
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("day")
 class DayResource {
-  //  @GET
-  // @Produces(MediaType.APPLICATION_JSON)
+//    @GET
+//  @Produces(MediaType.APPLICATION_JSON)
     public List findAll() {
         def result = findAllDays().collect { Day day ->
             day.id
@@ -22,6 +23,17 @@ class DayResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Day> findAllDays() {
-        [new Day(id: 1), new Day(id: 2), new Day(id: 3)]
+       [new Day(id: 1), new Day(id: 2), new Day(id: 3)]
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Day find(@PathParam("id") int id) {
+        List days = findAllDays()
+        if( days.size() > id ){
+            return days[id-1]
+        }
+        return null
     }
 }
